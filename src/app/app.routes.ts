87decf311f5b08b68966669e11fbaf2603b0698a
Nazar Herasymchuk <nazarii.herasymchuk@student.tuke.sk>
2024-11-08 17:01:53 +0100

    jhu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from "./components/home/home.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: 'home', component: HomeComponent }, // Directly loads HomeComponent
+  // Add more routes as needed
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
