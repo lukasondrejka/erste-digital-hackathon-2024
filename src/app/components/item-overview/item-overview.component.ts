7f64f36ef0acfb29ed82aa2bd7fb0d6a4b942b6d
Lukáš Ondrejka <lukas.ondrejka1@gmail.com>
2024-11-09 02:57:48 +0100

    Item overview page

--- src/app/components/item-overview/item-overview.component.ts
+++ src/app/components/item-overview/item-overview.component.ts
@@ -4,6 +4,7 @@ import { MistralaiService } from "../../services/mistralai.service";
 import { JsonDataServiceService } from "../../services/json-data-service.service";
 import { ActivatedRoute } from '@angular/router';
 import { generatePrompt } from "../../utils/prompt";
+import {Item} from "../../models/item";
 
 @Component({
   selector: 'app-item-overview',
@@ -17,6 +18,8 @@ export class ItemOverviewComponent implements OnInit {
   materials: Array<Material> = [];
   itemName: string = '';
   response: string = '';
+  item!: Item;
+  materialDescription: string = '';
 
   constructor(
     private mistrallaiService: MistralaiService,
@@ -30,17 +33,57 @@ export class ItemOverviewComponent implements OnInit {
     this.route.params.subscribe(async params => {
       this.itemName = params['itemName'];
 
-      try {
-        const materials =
-          await this.jsonDataService.getJsonData<Array<Material>>('data/materials.json').toPromise();
-        this.materials = <Array<Material>>materials;
+      this.item = {
+        name: 'bicycle',
+        materials: [
+          {
+            name: 'metal',
+            description: 'Metal is a material that is used in many items.',
+            impact: 'low',
+          },
+          {
+            name: 'rubber',
+            description: 'Rubber is a material that is used in many items.',
+            impact: 'medium',
+          },
+          {
+            name: 'plastic',
+            description: 'Plastic is a material that is used in many items.',
+            impact: 'high',
+          },
+          {
+            name: 'glass',
+            description: 'glass is a material that is used in many items.',
+            impact: 'high',
+          },
+        ],
+        reuse: ['reuse center'],
+        recycle: ['recycling center', 'landfill'],
+        valuable: true,
+      }
+      this.isLoading = false;
 
-        this.response = await this.mistrallaiService.sendMessage(generatePrompt(this.itemName));
-      } catch (error) {
-        console.error('Error loading data', error);
-      } finally {
-        this.isLoading = false;
+      // TODO: Implement loading data from JSON files and sending a message to MistralAI
+      // try {
+      //   const materials =
+      //     await this.jsonDataService.getJsonData<Array<Material>>('data/materials.json').toPromise();
+      //   this.materials = <Array<Material>>materials;
+      //
+      //   this.response = await this.mistrallaiService.sendMessage(generatePrompt(this.itemName));
+      // } catch (error) {
+      //   console.error('Error loading data', error);
+      // } finally {
+      //   this.isLoading = false;
+      // }
+
+      if (this.materials.length > 0) {
+        this.materialDescription = this.item.materials[0].description;
       }
     });
+  }
+
+  selectMaterial(materialName: string) {
+    this.materialDescription = this.item.materials.find(material =>
+      material.name === materialName)?.description || '';
   }
 }
