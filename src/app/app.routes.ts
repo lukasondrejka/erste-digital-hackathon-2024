346bd6580f12a56ca27f1a7f36e9dcd7ba0d20d7
Lukáš Ondrejka <lukas.ondrejka1@gmail.com>
2024-11-09 06:58:59 +0100

    Edit readme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import {ItemOverviewComponent} from "./components/item-overview/item-overview.component";
-import {CommonModule} from "@angular/common";
+import { ItemOverviewComponent } from "./components/item-overview/item-overview.component";
+import { CommonModule } from "@angular/common";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
