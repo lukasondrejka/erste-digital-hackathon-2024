a188c3d543455a5b5c73eb7be38455541688ba44
Lukáš Ondrejka <lukas.ondrejka1@gmail.com>
2024-11-09 01:49:09 +0100

    Edit item

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -1,6 +1,11 @@
+import {Material} from "./material";
+
 interface Item {
   name: string,
-  materials: Array<string>,
+  materials: Material[],
+  reuse: string[],
+  recycle: string[],
+  valuable: boolean,
 }
 
 export type { Item };
